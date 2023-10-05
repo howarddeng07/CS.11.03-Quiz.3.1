c9d8ae6dffe317093399e5ed525f8eb46b2aaac4
howarddeng <howardd2026@student.cis.edu.hk>
2023-10-05 14:21:24 +0800

    complete

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,67 @@
 public class Main {
 
-    // Question 1 - calculateGrade
+    public static char calculateGrade(int grade){
+        if (grade>=90){
+            return 'A';
+        }
+        else if (grade>=80&&grade<90){
+            return 'B';
+        }
+        else if (grade>=70&&grade<80){
+            return 'C';
+        }
+        else if (grade>=60&&grade<70){
+            return 'D';
+        }
+        else if (grade>=50&&grade<60){
+            return 'E';
+        }
+        return 'F';
+    }
 
     // Question 2 - fizzBuzz
+    public static String fizzBuzz(int value){
+        if (value % 3 == 0 && value % 5 == 0) {
+            return "fizzbuzz";
+        }
+        else if (value % 3 == 0) {
+            return "fizz";
+        }
+        else if (value % 5 == 0){
+            return "buzz";
+        }
+        return "unlucky";
+    }
 
     // Question 3 - frontBack
+    public static String frontBack(String original){
+        if (original.length() < 2){
+            return original;
+        }
+        String frontBack = original.substring(0, 2);
+        return frontBack + original + frontBack;
+    }
 
-    // Question 4 - twoAsOne
+    public static boolean twoAsOne(int a, int b, int c){
+        if ((a+b)==c){
+            return true;
+        }
+        else if ((a+c)==b){
+            return true;
+        }
+        else if ((b+c)==a){
+            return true;
+        }
+        return false;
+    }
 
-    // Question 5 - endUp
+    public static String endUp(String original){
+        if (original.length() < 3) {
+            return original.toUpperCase();
+        }
+        int splitPoint = original.length() - 3;
+        return original.substring(0, splitPoint) + original.substring(splitPoint).toUpperCase();
+    }
+
 
 }
